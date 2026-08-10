ab9f910ebace4c2afb970c4fc34e2f6467bcb014
nirumanii <kmani79@gmail.com>
2026-08-10 17:20:43 +0530

    Adding to github.

---
+++ src/test/java/com/mani/devops/EmployeeManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mani.devops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
